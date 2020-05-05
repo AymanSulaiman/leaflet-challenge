63f429003fbe0757d92d5f9700b46e0865fe3392
AymanSulaiman <saymansulaiman@gmail.com>
2020-05-05 16:28:17 -0700

    trying to do the coloured circles for the magnitude of the eartchquake, I need help

--- Leaflet-Level-1/static/js/logic.js
+++ Leaflet-Level-1/static/js/logic.js
@@ -1,33 +1,89 @@
+const tectonicPlatesLink = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json"
+
+// Link for the earthquakes  
+const geoLink = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson"
+
+// range of colours for the earthquake magnitude
+//
+// Yellow:#fffa00
+//        #ffd600
+//        #ffb000
+//        #ff8800
+//        #ff5a00
+// Red:   #ff0000
+
+const setColourScale = (mag) => {
+  switch(true){
+    case(mag>0 && mag<=1): return "#fffa00";
+    break;
+    case(mag>1 && mag<=2): return "#ffd600";
+    break;
+    case(mag>2 && mag<=3): return "#ffb000";
+    break;
+    case(mag>3 && mag<=4): return "#ff8800";
+    break;
+    case(mag>4 && mag<=5): return "#ff5a00";
+    break;
+    case(mag>6): return "#ff0000" 
+  }
+};
+
+markerSize = (magnitude) => magnitude / 40;
+
 function createMap(earthquakes){
     
      // Create the tile layer that will be the background of our map
-  var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
+    let light = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"http://mapbox.com\">Mapbox</a>",
     maxZoom: 18,
     id: "mapbox.light",
     accessToken: APIKEY
-  });
+    });
+
+    let dark = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.dark",
+    accessToken: APIKEY
+    });
+
+    let satellite = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.satellite",
+    accessToken: APIKEY
+    });
+
+    let pirate = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.pirates",
+    accessToken: APIKEY
+    });
 
   // Create a baseMaps object to hold the lightmap layer
-  var baseMaps = {
-    "Light Map": lightmap
+  let baseMaps = {
+    Light: light,
+    Dark: dark,
+    Satellite: satellite,
+    Pirate: pirate
   };
 
   // Create an overlayMaps object to hold the bikeStations layer
-  var overlayMaps = {
-    "Earthquakes": earthquakes
+  let overlayMaps = {
+    Earthquakes: earthquakes
   };
 
   // Create the map object with options
-  var map = L.map("map", {
+  let map = L.map("map", {
     center: [40.73, -74.0059],
     zoom: 4,
-    layers: [lightmap, earthquakes]
+    layers: [pirate, earthquakes]
   });
 
   // Create a layer control, pass in the baseMaps and overlayMaps. Add the layer control to the map
   L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
+    collapsed: true
   }).addTo(map);
 
 }
@@ -41,28 +97,28 @@ function createMarkers(response){
     let quakeMarkers = [];
 
     // looping through the stations array
-    for (var index = 0; index < quakes.length; index++) {
+    
+    for (let index = 0; index < quakes.length; index++) {
 
-        var quake = quakes[index].properties;
+        let quake = quakes[index].properties;
         
         let quakeGeo = quakes[index].geometry
 
         // For each station, create a marker and bind a popup with the station's name
-        var quakeMarker = L.marker([quakeGeo.coordinates[0], quakeGeo.coordinates[1]])
-          .bindPopup("<h3>" + quake.place + "<h3><h3>Magnitude: " + quake.type + "<h3>");
+        let quakeMarker = L.marker([quakeGeo.coordinates[1], quakeGeo.coordinates[0]])
+          .bindPopup("<h3>Magnitude: " + quake.mag + "<h3><h3>Location: " + quake.place + "<h3>");
     
         // Add the marker to the bikeMarkers array
         quakeMarkers.push(quakeMarker);
       }
 
-    createMap(L.layerGroup(quakeMarkers))
+  createMap(L.layerGroup(quakeMarkers))
 
 }
 
 
-// Link for the earthquakes  
-var link = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson"
 
 
-d3.json(link, createMarkers)
+
+d3.json(geoLink, createMarkers)
 
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # leaflet-challenge
 
-This challenge is to visualise earthquake data from USGS. This will be done by obtaining the JSON file from this [link](https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson) which shows recorded earthquakes over the last week and is continuously updated.  This will be be visualised via the latitude and longitude the GEOJSON file provides. The data also provides the magnitude; to show this, we can correlate the size of a magnitude with cirlces and the colour of the circles.  The technologies that we will use are HTML, CSS, Bootstrap, JavaScript, and Leaflet. Leaflet will allow me to create the markers for the earthquake locations and can provide the visualisation with a satalite, greyscale maps, outdoors map, faultlines, and earthquake layers.
+This challenge is to visualise earthquake data from USGS. This will be done by obtaining the JSON file from this [link](https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson) which shows recorded earthquakes over the last week and is continuously updated.  This will be be visualised via the latitude and longitude the GEOJSON file provides. The data also provides the magnitude; to show this, we can correlate the size of a magnitude with cirlces and the colour of the circles.  The technologies that we will use are HTML, CSS, Bootstrap, JavaScript, and Leaflet. Leaflet will allow me to create the markers for the earthquake locations and can provide the visualisation with a satalite, greyscale maps, outdoors map, faultlines, and earthquake layers.
 
 ## Goals:
 
 ### Level 1:
 
-  * Obtain and import the [dataset](https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson).
+  * Obtain and import the [dataset](https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson).
   * Create a map using leaflet to plot all of the earthquates based upon their latitudes and longitude.
   * Create data markers to reflect the magnitude of the earthquake. The darker and bigger the circles, the larger the magnitude.
   * Provide a lenged for the map data.
@@ -17,3 +17,12 @@ This challenge is to visualise earthquake data from USGS. This will be done by o
   * Provide tectonic plates in the map.
   * Provide a sattelite, greyscale, and outdoors maps.
   * Add layer controls to the maps.
+
+## Technologies used
+
+  * HTML
+  * CSS
+  * Javascript
+  * D3
+  * Leaflet
+  * Python server (for local testing)
